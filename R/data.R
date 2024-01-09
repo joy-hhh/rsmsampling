d9c48c596163438f4bca38b4433389f5548e4681
joy-hhh <joyhhh@outlook.kr>
2024-01-09 10:46:12 +0900

    data document

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Example of Revenue Journal Entry data
+#'
+#' A subset of data from the KICPA Cyber training center
+#'
+#' @format ## `pop`
+#' A data frame with 57,419 rows and 22 columns:
+#' \describe{
+#'   \item{전표일자}{Journal Entry date}
+#'   \item{전표번호}{Journal Entry number}
+#'   \item{차변금액}{Debit Record}
+#'   \item{대변금액}{Credit Record}
+#'   ...
+#' }
+#' @source <https://www.kicpa.or.kr/>
+"pop"
